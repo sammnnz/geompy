bfd4b56af5e32099f20171403d69d53906df6597
Semen <drozdosam@mail.ru>
2022-12-27 01:28:26 +0300

    Create tests

---
+++ tests/test_geompy.py
@@ -0,0 +1,39 @@
+# noinspection SpellCheckingInspection
+"""
+Tests for geompy.py
+"""
+import geompy.geompy as gm
+# noinspection PyPackageRequirements
+import pytest
+
+
+@pytest.fixture(scope='session')
+def render_object():
+    # noinspection SpellCheckingInspection
+    """
+    Fixture, which create instance of geompy.Render class
+    """
+    r = gm.Render()
+    yield r
+    del r
+
+
+@pytest.mark.parametrize("x, y",
+                         [
+                             (1, 2),
+                             (-2.1, 0.5)
+                         ]
+                         )
+def test_Render_display(render_object, x, y):
+    render_object.display(x, y)
+    assert True is True
+
+
+@pytest.mark.parametrize("p, c, s",
+                         [
+                             (gm.point(0, 0), 'orange', 50.3)
+                         ]
+                         )
+def test_Render_points(render_object, p, c, s):
+    render_object.points(p, c, s)
+    assert True is True
