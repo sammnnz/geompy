9850698dc899d0e107cd915b21fd39c2598a065c
sammnnz <104736093+sammnnz@users.noreply.github.com>
2022-12-28 02:44:54 +0300

    Fix `M_PI` undeclared (for gcc)
    src/_geompy/resources/rand.c:15:25: error: ‘M_PI’ undeclared (first use in this function)

--- src/_geompy/resources/rand.c
+++ src/_geompy/resources/rand.c
@@ -11,6 +11,10 @@ double random_(double start, double stop) {
 	return start + (stop - start) * ((double)rand() / RAND_MAX);
 };
 
+#ifndef M_PI
+#	define M_PI 3.14159265358979323846   // pi
+#endif
+
 double random_angle(double start) {
 	if (start > 2 * M_PI) return 0.0;
 
