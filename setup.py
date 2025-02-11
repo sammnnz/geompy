f0b52d5daa029094b25e1e6117a003110007c722
sammnnz <104736093+sammnnz@users.noreply.github.com>
2025-02-11 19:04:52 +0300

    Changes for setuptools

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ from distutils.core import setup, Extension
 
 if __name__ == '__main__':
     setup(ext_modules=[Extension(name='_geompy',
-                                 sources=['src/_geompy/geompy.c',
+                                 sources=['src/_geompy/_geompy.c',
                                           'src/_geompy/objects/polygonobject.c'],
                                  include_dirs=['src/_geompy/include'],
-                                 extra_compile_args=['-std=c99']
+                                 extra_compile_args=['--std=c99', '--coverage'],
+                                 language="c"
                                  )
                        ]
           )
